cb87fc256ae631417911a077dd977da077389207
gomeliGithub <irina01041971@mail.ru>
2023-09-29 15:55:43 +0300

    Main Commit

--- nest-ssr/src/app/services/admin-panel/admin-panel.service.ts
+++ nest-ssr/src/app/services/admin-panel/admin-panel.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+
+import { AppService } from '../../app.service';
 
 import { ICompressedImage } from 'types/global';
 
@@ -9,20 +11,25 @@ import { ICompressedImage } from 'types/global';
 })
 export class AdminPanelService {
     constructor (
-        private readonly http: HttpClient
+        private readonly http: HttpClient,
+
+        private readonly appService: AppService
     ) { }
 
     public getFullCompressedImagesList (): Observable<ICompressedImage[]> {
-        return this.http.get('/api/admin-panel/getFullCompressedImagesList', { withCredentials: true }).pipe(map(imagesList => imagesList)) as Observable<ICompressedImage[]>;
+        const headers: HttpHeaders = this.appService.createRequestHeaders();
+
+        return this.http.get('/api/admin-panel/getFullCompressedImagesList', { headers, withCredentials: true }).pipe(map(imagesList => imagesList)) as Observable<ICompressedImage[]>;
     }
 
     public uploadImage (imageFile: File): void {
         const reader = new FileReader();
 
         reader.onload = event => {
+            const headers: HttpHeaders = this.appService.createRequestHeaders();
             const imageData: ArrayBuffer = (event.target as FileReader).result as ArrayBuffer;
 
-            this.http.post(`/api/client/uploadImage/:${imageFile.name}`, imageData).subscribe(result => result);
+            this.http.post(`/api/client/uploadImage/:${imageFile.name}`, imageData, { headers, withCredentials: true }).subscribe(result => result);
         }
 
         reader.readAsArrayBuffer(imageFile);
