fadeb5a267749c4649dbb91ee1ee4d3a0a98da95
gomeliGithub <irina01041971@mail.ru>
2024-01-26 23:33:57 +0300

    Main Commit

--- nest-ssr/src/environments/environment.ts
+++ nest-ssr/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { IClientLocale } from "types/global";
 
 export const environment = {
     production: true,
-    webSocketServerURL: 'ws://178.172.173.222:83',
+    webSocketServerURL: 'ws://45.128.205.220:83',
     locales: [
         { code: 'ru', title: 'Русский' }, 
         { code: 'en', title: 'English'}
